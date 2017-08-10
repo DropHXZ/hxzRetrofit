85d36f7cafeeac665d11271d86269ad3558f61eb
huxiaozong <message_sky@126.com>
2017-08-10 16:26:12 +0800

    git仓库测试

--- app/src/main/java/cn/hxzretrofit/MainActivity.kt
+++ app/src/main/java/cn/hxzretrofit/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : Activity() {
 
+    var h:Int = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
